a2e14db1098edad9703bedf19187c77e1adf06b2
hewhodevs <hewhodevs@outlook.com>
2022-05-17 11:55:24 +0930

    Created UserItem Component - added component state - added hardcoded state values for testing - added styles

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,8 +1,33 @@
 import React, { Component } from 'react';
 
 class UserItem extends Component {
+  state = {
+    id: 'id',
+    login: 'mojombo',
+    avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
+    html_url: 'https://github.com/mojombo',
+  };
+
   render() {
-    return <div>UserItem</div>;
+    // destructured assignment, so we can use itemName, rather than this.state.itemName below
+    const { login, avatar_url, html_url } = this.state;
+    return (
+      <div className='card text-center'>
+        <img
+          src={avatar_url}
+          alt=''
+          className='round-img'
+          style={{ width: '60px' }}
+        />
+        <h3>{login}</h3>
+
+        <div>
+          <a href={html_url} className='btn btn-dark btm-sm my-1'>
+            More
+          </a>
+        </div>
+      </div>
+    );
   }
 }
 
