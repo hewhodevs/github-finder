a403536d96e469554b5fbf8a2acd1a813e94dacb
hewhodevs <hewhodevs@outlook.com>
2022-05-17 14:11:19 +0930

    Moved user data to application state level - get users with axios at the application level - user data now passed down as a prop to UserList Component

--- src/App.js
+++ src/App.js
@@ -5,14 +5,26 @@ import axios from 'axios';
 import './App.css';
 
 class App extends Component {
-  componentDidMount() {}
+  state = {
+    users: [],
+    loading: false,
+  };
+
+  async componentDidMount() {
+    // show loading while getting user data
+    this.setState({ loading: true });
+    // get user data
+    const res = await axios.get('https://api.github.com/users');
+    // Stop showing loading once we have our user data
+    this.setState({ users: res.data, loading: false });
+  }
 
   render() {
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
