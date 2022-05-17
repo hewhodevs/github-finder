e0d9e26f120121b0cf15f7df6e6e9ba305dfc879
hewhodevs <hewhodevs@outlook.com>
2022-05-17 13:56:22 +0930

    installed axios dependency

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {}
+
   render() {
     return (
       <div className='App'>
