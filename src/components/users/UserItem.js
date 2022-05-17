7ab9b7edf7a96f67d4dd4efdda60d59fcdac1411
hewhodevs <hewhodevs@outlook.com>
2022-05-17 13:41:44 +0930

    UserItem converted to functional component - As stateless, converted to functional component instead

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,26 +1,22 @@
-import React, { Component } from 'react';
+const UserItem = (props) => {
+  const { login, avatar_url, html_url } = props.user;
+  return (
+    <div className='card text-center'>
+      <img
+        src={avatar_url}
+        alt=''
+        className='round-img'
+        style={{ width: '60px' }}
+      />
+      <h3>{login}</h3>
 
-class UserItem extends Component {
-  render() {
-    const { login, avatar_url, html_url } = this.props.user;
-    return (
-      <div className='card text-center'>
-        <img
-          src={avatar_url}
-          alt=''
-          className='round-img'
-          style={{ width: '60px' }}
-        />
-        <h3>{login}</h3>
-
-        <div>
-          <a href={html_url} className='btn btn-dark btm-sm my-1'>
-            More
-          </a>
-        </div>
+      <div>
+        <a href={html_url} className='btn btn-dark btm-sm my-1'>
+          More
+        </a>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default UserItem;
