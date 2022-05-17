d210c74ae0618848610b1a98d55a3fd0beaabdcf
Joshua Kelly <hewhodevs@outlook.com>
2022-05-17 15:24:13 +0930

    Added Search Component - Created Search.js - Added Search.js to App.js - Creates generic onChange handler function in Search.js

---
+++ src/components/users/Search.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+  state = {
+    text: '',
+  };
+
+  onChange = (e) => {
+    // gets the name of the event firing field, and sets it's value in state
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  render() {
+    return (
+      <div>
+        <form className='form'>
+          <input
+            type='text'
+            name='text'
+            placeholder='Search Users...'
+            value={this.state.text}
+            onChange={this.onChange}
+          />
+          <input
+            type='submit'
+            value='Search'
+            className='btn btn-dark btn-block'
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Search;
