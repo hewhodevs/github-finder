ce32d49399a9e493d1e1cf438146c07500b76dbd
Joshua Kelly <hewhodevs@outlook.com>
2022-05-19 10:44:00 +0930

    Added showClear flag prop to App.js - Flag boolean used to indicate if we need to show clear button or not - If we have users, passes down true to Search.js to show clear button - added showClear as a required bool propType in Search.js - Wrapped Search.js button with Null coalescing operator to toggle button visibility

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -9,6 +9,7 @@ class Search extends Component {
   static propTypes = {
     searchUsers: PropTypes.func.isRequired,
     clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
   };
 
   onChange = (e) => {
@@ -39,12 +40,14 @@ class Search extends Component {
             className='btn btn-dark btn-block'
           />
         </form>
-        <button
-          className='btn btn-light btn-block'
-          onClick={this.props.clearUsers}
-        >
-          Clear
-        </button>
+        {this.props.showClear && (
+          <button
+            className='btn btn-light btn-block'
+            onClick={this.props.clearUsers}
+          >
+            Clear
+          </button>
+        )}
       </div>
     );
   }
