08c82246f777ea420b9a574250d5787574c6ca6a
Joshua Kelly <hewhodevs@outlook.com>
2022-05-19 10:51:12 +0930

    Refactored App.js with destructuring of this.state - destructuring used in render method to pull out users and loading - removed 3 references to this.state where no longer needed in render method

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ class App extends Component {
   };
 
   render() {
+    const { users, loading } = this.state;
     return (
       <div className='App'>
         <Navbar />
@@ -32,9 +33,9 @@ class App extends Component {
           <Search
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
-            showClear={this.state.users.length > 0 ? true : false}
+            showClear={users.length > 0 ? true : false}
           />
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );
